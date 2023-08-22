d9392faecdd1753edb318840b6580a2c48e50e59
AdaiasSouza <adaias_305@hotmail.com>
2023-08-22 17:01:50 -0300

    Inclusão dos métodos para o cálculo de normalização

--- Normalizacao/normalizacao.py
+++ Normalizacao/normalizacao.py
@@ -1,5 +1,14 @@
 def norma_min_max(atributes, new_min, new_max):
-	pass
+	for row in atributes:
+		part_one = ((row - min(atributes))/(max(atributes)-min(atributes)))
+		part_two = (new_max-new_min) + new_min
+
+		norma_value = part_one*part_two
+
+		return norma_value
 
 def norma_z_score(atributes):
-	pass 
+	 
+
+
+
