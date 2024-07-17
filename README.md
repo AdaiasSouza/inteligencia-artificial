d84c6d625adba02de46af484c4cdf87c8ec7d69b
AdaiasSouza <adaias_305@hotmail.com>
2024-07-17 16:22:09 -0300

    atualizando Readme e criação de classe para normalização

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Inteligencia-Artificial
 Reposítorio da disciplina de Inteligência Artificial
+1. Distâncias:
+     Euclidiana
+     Manhattan
+     Chebyshev
+
+2. Normalização:
+
--- normalizacao/normalizacao.py
+++ normalizacao/normalizacao.py
@@ -1,14 +1,37 @@
-def norma_min_max(atributes, new_min, new_max):
-	for row in atributes:
-		part_one = ((row - min(atributes))/(max(atributes)-min(atributes)))
-		part_two = (new_max-new_min) + new_min
+class Normalizacao():
 
-		norma_value = part_one*part_two
+    """docstring for Normalizacao"""
 
-		return norma_value
+    def __init__(self, atribute):
+        self.atribute = atribute
 
-def norma_z_score(atributes):
-	 
+    def norma_min_max(atribute, new_min, new_max):
+
+        for row in atribute:
+            part_one = (
+                (row - min(atribute)) / (max(atribute) - min(atribute)))
+            part_two = (new_max - new_min) + new_min
+
+            norma_value = part_one * part_two
+
+            return norma_value
+
+    def norma_z_score(atributes):
+        pass
+
+    def normal_decimal():
+        pass
+
+    def norma_robust():
+        pass
+
+    def norma_log():
+        pass
+
+    def __str__(self):
+        pass
 
 
-
+x = [1, 2, 4, 100, 34]
+c = Normalizacao(x)
+print(c)
